a470700351261dbbb28a2fcf74c15cb4b0aa5487
kephin <wunshon76325@gmail.com>
2020-08-01 21:13:34 +0800

    feat: add /events for comments, posts services

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -11,17 +12,32 @@ const PORT = 4001
 
 const commentsByPostId = {}
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(6).toString('hex')
   const newComment = { id: commentId, content: req.body.content }
   const comments = commentsByPostId[req.params.id] || []
   comments.push(newComment)
   commentsByPostId[req.params.id] = comments
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      ...newComment,
+      postId: req.params.id
+    }
+  })
+
   res.status(201).send(comments)
 })
 
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || [])
+})
+
+app.post('/events', (req, res) => {
+  console.log('====================================');
+  console.log(req.body);
+  console.log('====================================');
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -14,15 +15,27 @@ const posts = [
   { id: 2, title: 'second post' }
 ]
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(6).toString('hex')
   const newPost = { id, title: req.body.title }
   posts.push(newPost)
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: newPost
+  })
+
   res.status(201).send(newPost)
 })
 
 app.get('/posts', (req, res) => {
   res.status(200).send(posts)
+})
+
+app.post('/events', (req, res) => {
+  console.log('====================================');
+  console.log(req.body);
+  console.log('====================================');
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
