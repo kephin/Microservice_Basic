725ebe2914ea2a1cdec313c3a1c535b076125bdc
kephin <wunshon76325@gmail.com>
2020-08-02 15:53:21 +0800

    feat: react app gets posts from query service

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,19 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-export default ({ postId }) => {
-  const [comments, setComments] = useState([])
-  const fetchComments = async () => {
-    const { data } = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-    setComments(data)
-  }
-  useEffect(() => {
-    fetchComments()
-  })
-
-  const renderComments = comments.map(comment => (
-    <li key={comment.id}>{comment.content}</li>
-  ))
-
-  return <ul>{renderComments}</ul>
-}
+export default ({ comments }) => (
+  <ul>
+    {comments.map(comment => (
+      <li key={comment.id}>{comment.content}</li>
+    ))}
+  </ul>
+)
